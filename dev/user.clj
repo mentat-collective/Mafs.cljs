1ae7a570ff6e9a8836dcee515e01e295d9fdcd94
Sam Ritchie <sritchie09@gmail.com>
2023-01-30 13:47:52 -0700

    tidy

--- dev/user.clj
+++ dev/user.clj
@@ -9,24 +9,32 @@
    :browse? true
    :watch-paths ["dev"]
    :cljs-namespaces '[mafs.clerk-ui]
+   :cname "mafs.mentat.org"
    :github-slug "mentat-collective/mafs.cljs"})
 
-(defonce _ignore
-  (b/add-css!
-   "https://unpkg.com/computer-modern@0.1.2/cmu-serif.css"
-   "https://unpkg.com/mafs@0.11.4/core.css"
-   "https://unpkg.com/mafs@0.11.4/font.css"))
+(def static-defaults
+  (assoc defaults :browse? false))
+
+(b/set-css!
+ "https://unpkg.com/computer-modern@0.1.2/cmu-serif.css"
+ "https://unpkg.com/mafs@0.11.4/core.css"
+ "https://unpkg.com/mafs@0.11.4/font.css")
 
 (defn start!
   ([] (start! {}))
   ([opts]
    (b/serve!
-    (merge defaults opts))))
+    (merge defaults
+           {:browse? true}
+           opts))))
 
 (def halt! b/halt!)
 
 (defn static-build! [opts]
-  (b/static-build! (merge defaults opts)))
+  (b/static-build!
+   (merge static-defaults
+          opts)))
 
 (defn garden! [opts]
-  (b/garden! (merge defaults opts)))
+  (b/garden!
+   (merge static-defaults opts)))
