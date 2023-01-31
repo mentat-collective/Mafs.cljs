015e088c7f82da816b0afc09e56fec348bc7cd7e
Sam Ritchie <sritchie09@gmail.com>
2023-01-30 20:57:47 -0700

    simplify further

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,11 @@
 (ns user
   (:require [mentat.clerk-utils.build :as b]))
 
+(b/set-css!
+ "https://unpkg.com/computer-modern@0.1.2/cmu-serif.css"
+ "https://unpkg.com/mafs@0.11.4/core.css"
+ "https://unpkg.com/mafs@0.11.4/font.css")
+
 (def index
   "dev/mafs/notebook.clj")
 
@@ -8,33 +13,22 @@
   {:index index
    :browse? true
    :watch-paths ["dev"]
-   :cljs-namespaces '[mafs.clerk-ui]
-   :cname "mafs.mentat.org"
-   :github-slug "mentat-collective/mafs.cljs"})
+   :cljs-namespaces '[mafs.clerk-ui]})
 
 (def static-defaults
-  (assoc defaults :browse? false))
-
-(b/set-css!
- "https://unpkg.com/computer-modern@0.1.2/cmu-serif.css"
- "https://unpkg.com/mafs@0.11.4/core.css"
- "https://unpkg.com/mafs@0.11.4/font.css")
+  (assoc defaults
+         :browse? false
+         :cname "mafs.mentat.org"
+         :git/url "https://github.com/mentat-collective/mafs.cljs"))
 
 (defn start!
   ([] (start! {}))
   ([opts]
    (b/serve!
-    (merge defaults
-           {:browse? true}
-           opts))))
+    (merge defaults opts))))
 
 (def halt! b/halt!)
 
 (defn static-build! [opts]
   (b/static-build!
-   (merge static-defaults
-          opts)))
-
-(defn garden! [opts]
-  (b/garden!
    (merge static-defaults opts)))
